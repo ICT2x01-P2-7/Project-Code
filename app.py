6c6864da47314b4dc7527963408c03ff838f2e45
Ryan <2000507@sit.singaporetech.edu.sg>
2021-09-14 08:29:50 +0800

    Added backend code for page navigation

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) #creating the Flask class object   
+app = Flask(__name__) # Create the flask object  
  
-@app.route('/') #decorator drfines the   
-def home():  
-    return render_template('index.html') 
-  
+@app.route('/')   
+def default():  
+    return render_template('index.html')
+
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/advanced.html')
+def advanced():
+    return render_template('advanced.html')
+
+@app.route('/beginner.html')
+def beginner():
+    return render_template('beginner.html')
+
+@app.route('/statistics.html')
+def statistics():
+    return render_template('statistics.html')
+
 if __name__ =='__main__':  
     app.run(debug = True)
