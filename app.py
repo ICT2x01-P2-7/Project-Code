ae0e6ec70954ab78f3d82fa0d005d8afd03f92eb
Ryan <2000507@sit.singaporetech.edu.sg>
2021-11-03 17:43:33 +0800

    Instructor password authentication working. Still need figure out the error message though

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+import hashlib
 
 app = Flask(__name__) # Create the flask object  
  
@@ -10,18 +13,28 @@ def default():
 def home():
     return render_template('index.html')
 
-@app.route('/advanced.html')
-def advanced():
-    return render_template('advanced.html')
+@app.route('/connect.html', methods=['POST'])
+def connect():
+    error = "none"
+    password = request.form.get('instructorPW')
+    print(password)
 
-@app.route('/beginner.html')
-def beginner():
-    return render_template('beginner.html')
+    # Hash the password for comparison
+    encoded = password.encode()
+    result = hashlib.sha256(encoded)
+    hexadecimal = result.hexdigest()
 
-@app.route('/statistics.html')
-def statistics():
-    # Code here needs to pull data from car
-    return render_template('statistics.html')
+    print(hexadecimal)
+
+    if hexadecimal != "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918":
+        error = "Invalid password!"
+        return redirect(url_for('home'))
+
+    return render_template('connect.html', error=error)
+
+@app.route('/game.html')
+def game():
+    return render_template('game.html')
 
 @app.route('/upload.html')
 def upload():
