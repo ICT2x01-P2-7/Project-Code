e110dd124e34d3912af8d5eaa0c9239e714b5272
Ryan <2000507@sit.singaporetech.edu.sg>
2021-11-04 13:15:41 +0800

    Added some error checking

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask.helpers import url_for
+from flask.helpers import flash, url_for
 from werkzeug.utils import redirect
 import hashlib
 
@@ -15,7 +15,7 @@ def home():
 
 @app.route('/connect.html', methods=['POST'])
 def connect():
-    error = "none"
+    error = None
     password = request.form.get('instructorPW')
     print(password)
 
@@ -27,9 +27,11 @@ def connect():
     print(hexadecimal)
 
     if hexadecimal != "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918":
-        error = "Invalid password!"
-        return redirect(url_for('home'))
-
+        error = 'Invalid Password!'
+        print(error)
+        #return redirect(url_for('home', error=error))
+        return render_template('index.html', error=error)
+    print(error)
     return render_template('connect.html', error=error)
 
 @app.route('/game.html')
@@ -38,9 +40,35 @@ def game():
 
 @app.route('/challenge.html', methods=['POST'])
 def challenge():
+    error = None
     addr = request.form.get("carIP")
     print(addr)
+
+    check = validate_ip_address(addr)
+    if check == False:
+        error = 'Invalid IP Address!'
+        return render_template('connect.html', error=error)
+
     return render_template('challenge.html')
+
+def validate_ip_address(address):
+    parts = address.split(".")
+
+    if len(parts) != 4:
+        print("IP address {} is not valid".format(address))
+        return False
+
+    for part in parts:
+        if not isinstance(int(part), int):
+            print("IP address {} is not valid".format(address))
+            return False
+
+        if int(part) < 0 or int(part) > 255:
+            print("IP address {} is not valid".format(address))
+            return False
+ 
+    print("IP address {} is valid".format(address))
+    return True 
 
 @app.route('/upload.html')
 def upload():
