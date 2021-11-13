f1209debedb1a17b50a6cb0aec3b5cc006433918
Ryan <2000507@sit.singaporetech.edu.sg>
2021-11-13 23:50:08 +0800

    Deleted the unessasary files

--- test_app.py
+++
@@ -1,6 +0,0 @@
-def test_connect(self):
-    assert self.hexadecimal == '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
-
-
-def test_challenge(self):
-    assert self.error == None
