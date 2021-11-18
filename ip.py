b914b45ab14ae1fcdcf160d8902a748bfb439b23
Ryan <2000507@sit.singaporetech.edu.sg>
2021-11-18 14:21:48 +0800

    Updated the code with comments and also fixed a bug with the connect method

--- ip.py
+++ ip.py
@@ -1,6 +1,9 @@
 import socket
 import requests
 
+# This method checks to make sure that the input from the user is valid
+# Input: String
+# Output: Boolean
 def validate_ip_address(address):
     parts = address.split(".")
 
@@ -20,13 +23,16 @@ def validate_ip_address(address):
     #print("IP address {} is valid".format(address))
     return True
 
+# This method actually starts the connection with the car
+# Input: String
+# Output: Boolean
 def start_connect(address):
     URL = "http://" + address + "/"
     response = requests.get(URL)
     print(response.status_code)
     print(response.text)
 
-    if response.status_code == True:
+    if response.status_code == 200:
         return True
     else:
         return False        
