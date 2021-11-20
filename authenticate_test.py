10fbe3776a5fffff31af8ebfd808c939dd8d1710
Ryan <2000507@sit.singaporetech.edu.sg>
2021-11-20 22:27:14 +0800

    Updated coverage report

--- authenticate_test.py
+++ authenticate_test.py
@@ -3,25 +3,31 @@ from authenticate import check
 def test1():
     result = check('password')
 
-    print("Test 1: Passed in 'password', expect False")
+    print("Test 1: Passed in string 'password', expect False")
     print("Result returned: ", result)
 
 def test2():
     result = check(123)
 
-    print("Test 2: Passed in number 123, expect False")
+    print("Test 2: Passed in integer 123, expect False")
     print("Result returned: ", result)
 
 def test3():
     result = check('1a2b3c')
 
-    print("Test 3: Gave '1a2b3c', expect False")
+    print("Test 3: Passed in string '1a2b3c', expect False")
     print("Result returned: ", result)
 
 def test4():
     result = check('admin')
 
-    print("Test 4: Gave 'admin', expect True")
+    print("Test 4: Passed in string 'admin', expect True")
+    print("Result returned: ", result)
+
+def test5():
+    result = check(123.123)
+
+    print("Test 5: Passed in float 123.123, expect False")
     print("Result returned: ", result)
 
 if __name__ =='__main__':
@@ -32,3 +38,5 @@ if __name__ =='__main__':
     test3()
     print('\n')
     test4()
+    print('\n')
+    test5()
